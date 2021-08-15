54132204c8689c8c296f87a1173e2b5084da2fc6
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-08-15 13:08:19 -0300

    Desativando mensagem de log

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, LogBox } from 'react-native';
 
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
@@ -11,6 +11,8 @@ import { Background } from './src/components/Background';
 
 import { Routes } from './src/routes';
 import { AuthProvider } from './src/contexts/AuthContext';
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
